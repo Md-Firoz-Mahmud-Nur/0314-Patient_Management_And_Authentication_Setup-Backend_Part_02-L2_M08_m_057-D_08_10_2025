b28326ac40be9f7f136d1262c2e6d6d6dcc18ae5
firoz.nur <firoznur5@gmail.com>
2025-10-17 11:04:46 +0600

    Add : Set Cookies

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -6,11 +6,27 @@ import { AuthService } from "./auth.service";
 const login = catchAsync(async (req: Request, res: Response) => {
   const result = await AuthService.login(req.body);
 
+  const { accessToken, refreshToken, needPasswordChange } = result;
+
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+    maxAge: 1000 * 60 * 60,
+  });
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+    maxAge: 1000 * 60 * 60 * 24 * 90,
+  });
+
   sendResponse(res, {
     statusCode: 201,
     success: true,
     message: "User logged successfully!",
-    data: result,
+    data: { needPasswordChange },
   });
 });
 
