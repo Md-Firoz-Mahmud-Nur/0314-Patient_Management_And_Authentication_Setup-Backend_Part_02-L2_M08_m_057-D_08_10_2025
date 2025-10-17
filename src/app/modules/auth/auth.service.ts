a3ccd9396d6da708f72f97b301de5f2313af3b7f
firoz.nur <firoznur5@gmail.com>
2025-10-17 10:29:38 +0600

    Add : Password Check

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,5 +1,26 @@
+import { UserStatus } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import { prisma } from "../../shared/prisma";
+
 const login = async (payload: { email: string; password: string }) => {
-  console.log(payload);
+  console.log("user login info", payload);
+  const user = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: payload.email,
+      status: UserStatus.ACTIVE,
+    },
+  });
+
+  const isPasswordMatched = await bcrypt.compare(
+    payload.password,
+    user.password
+  );
+
+  if (!isPasswordMatched) {
+    throw new Error("Invalid email or password");
+  }
+
+  return user;
 };
 
 export const AuthService = {
